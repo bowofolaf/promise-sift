304f5ba91bd290b9e515ed75da1bae8f21613464
Bowofola <bowofola@gmail.com>
2017-06-21 00:14:19 -0400

    feat(main)
    restrict to return only successful promises and add tests for error handler.

--- src/promise-any.js
+++ src/promise-any.js
@@ -5,21 +5,28 @@
     Promise.any = promiseAny;
 
     function promiseAny(promises, errorHandler){
+        var results = [];
+
         if(!promises){
             throw new Error('promises cannot be null or undefined.');
         }
 
         // mutate promises into new list with errors swallowed.
         var resolvedPromises = promises.map(function (promise){
+            // store results of successful promises
             // execute error handler on any promise that fails
-            return promise.catch(function (err){
-                if(errorHandler){
-                    errorHandler(err);
-                }
-            });
+            return promise
+                .then(Array.prototype.push.bind(results))
+                .catch(function (reason){
+                    if(errorHandler){
+                        errorHandler(reason);
+                    }
+                });
         });
 
-        return Promise.all(resolvedPromises);
+        return Promise.all(resolvedPromises).then(function (){
+            return results;
+        });
     }
 
 })();
--- test/promise-any.spec.js
+++ test/promise-any.spec.js
@@ -4,6 +4,7 @@ describe('Promise any', function (){
     });
 
     it('should return list of results for successful promises', function (done){
+        // wood
         var promises = [
             Promise.resolve('success1'),
             Promise.resolve('success2'),
@@ -13,16 +14,36 @@ describe('Promise any', function (){
 
         var _results;
 
+        // fire
         Promise.any(promises).then(function (results){
             _results = results;
-        }).catch(function (error){
-
         });
 
+        // bellow
         setTimeout(function (){
             expect(_results).toBeDefined();
-            expect(_results.length).toBe(4);
+            expect(_results.length).toBe(3);
             done();
+        }, 50);
+    });
+
+    it('should execute error handler for each failed promise', function (){
+        // wood
+        var promises = [
+            Promise.resolve('success1'),
+            Promise.reject('failure1'),
+            Promise.reject('failure2'),
+            Promise.resolve('success2')
+        ];
+        var errorHandler = jasmine.createSpy('errorHandler');
+
+        // fire
+        Promise.any(promises, errorHandler).then(function (results){
+        });
+
+        // bellow
+        setTimeout(function (){
+            expect(errorHandler).toHaveBeenCalledTimes(2);
         }, 50);
     });
 });
